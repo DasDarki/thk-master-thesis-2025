ef5ac7a18a1b25da8c4535d33eca826715957474
DasDarki <rageskilln@gmail.com>
2025-03-23 18:18:03 +0100

    fix: append to csv

--- collector/csv.go
+++ collector/csv.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -16,8 +17,19 @@ func saveToEmergencyCsv(run *TestRun) error {
 }
 
 func writeToCsv(file string, run *TestRun) error {
-	if err := createCsvHeaderIfNotExists(file); err != nil {
-		return err
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		log.Println("Creating new CSV file", file)
+		parts := []string{
+			"id", "protocol", "enviroment", "time_slot", "test_begin", "test_end", "client_id", "parallel_clients",
+			"transfer_start_unix", "transfer_end_unix", "latency_ms", "throughput_mbps", "bytes_sent_total", "bytes_payload", "bandwidth_efficiency",
+			"cpu_client_percent_before", "cpu_client_percent_after", "cpu_client_percent_while", "cpu_server_percent_before", "cpu_server_percent_after", "cpu_server_percent_while",
+			"ram_client_bytes_before", "ram_client_bytes_after", "ram_client_bytes_while", "ram_server_bytes_before", "ram_server_bytes_after", "ram_server_bytes_while",
+			"lost_packets", "retransmissions", "connection_duration", "stream_duration", "error",
+		}
+
+		if err := os.WriteFile(file, []byte(strings.Join(parts, ";")+"\n"), 0644); err != nil {
+			return err
+		}
 	}
 
 	data := []string{
@@ -28,20 +40,19 @@ func writeToCsv(file string, run *TestRun) error {
 		fmt.Sprintf("%d", run.Data.LostPackets), fmt.Sprintf("%d", run.Data.Retransmissions), fmt.Sprintf("%d", run.Data.ConnectionDuration), fmt.Sprintf("%d", run.Data.StreamDuration), run.Data.Error,
 	}
 
-	return os.WriteFile(file, []byte(strings.Join(data, ",")+"\n"), os.ModeAppend)
+	return appendToEndOfFile(file, strings.Join(data, ";")+"\n")
 }
 
-func createCsvHeaderIfNotExists(file string) error {
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		parts := []string{
-			"id", "protocol", "enviroment", "time_slot", "test_begin", "test_end", "client_id", "parallel_clients",
-			"transfer_start_unix", "transfer_end_unix", "latency_ms", "throughput_mbps", "bytes_sent_total", "bytes_payload", "bandwidth_efficiency",
-			"cpu_client_percent_before", "cpu_client_percent_after", "cpu_client_percent_while", "cpu_server_percent_before", "cpu_server_percent_after", "cpu_server_percent_while",
-			"ram_client_bytes_before", "ram_client_bytes_after", "ram_client_bytes_while", "ram_server_bytes_before", "ram_server_bytes_after", "ram_server_bytes_while",
-			"lost_packets", "retransmissions", "connection_duration", "stream_duration", "error",
-		}
+func appendToEndOfFile(file string, data string) error {
+	f, err := os.OpenFile(file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
 
-		return os.WriteFile(file, []byte(strings.Join(parts, ";")+"\n"), 0644)
+	defer f.Close()
+
+	if _, err = f.WriteString(data); err != nil {
+		return err
 	}
 
 	return nil
